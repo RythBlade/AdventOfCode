eccbc6be096ec5a46b113967400dcd634b4d1d57
RythBlade <redeyescf@hotmail.com>
2020-12-04 17:40:41 +0000

    Part 2 complete and correct

--- year_2020/advent_of_code_2020_day_04/advent_of_code_2020_day_04/advent_of_code_2020_day_04.py
+++ year_2020/advent_of_code_2020_day_04/advent_of_code_2020_day_04/advent_of_code_2020_day_04.py
@@ -16,18 +16,23 @@ passports = passport.parsePassportsFromLines(passportFile)
 
 print("Passports found, {0}".format(len(passports)))
 
-validPassports = 0
+validPassportsPart1 = 0
+validPassportsPart2 = 0
 passportsThatFailedToParse = 0
 
-for nextPassport in passports:
-    if nextPassport.parsedSuccess:
-        if nextPassport.isPassportValid():
-            validPassports += 1
-        else:
-            print("invalid, {0}".format(nextPassport._Passport__passportString))
+passportId = 0
+
+for passportId in range(len(passports)):
+    if passports[passportId].parsedSuccess:
+        if passports[passportId].isPassportValidPart1():
+            validPassportsPart1 += 1
+
+        if passports[passportId].isPassportValidPart2():
+            validPassportsPart2 += 1
     else:
         passportsThatFailedToParse += 1
 
 print("Passports failed to parse, {0}".format(passportsThatFailedToParse))
-print("Valid Passports, {0}".format(validPassports))
+print("Valid Passports part 1, {0}".format(validPassportsPart1))
+print("Valid Passports part 2, {0}".format(validPassportsPart2))
 print("Application complete. Time taken(s), {0}".format(applicationTimer.elapsedTime()))
--- year_2020/advent_of_code_2020_day_04/advent_of_code_2020_day_04/passport.py
+++ year_2020/advent_of_code_2020_day_04/advent_of_code_2020_day_04/passport.py
@@ -21,7 +21,7 @@ class Passport:
             else:
                 self.__dataDictionary[splitPair[0]] = splitPair[1]
 
-    def isPassportValid(self):
+    def isPassportValidPart1(self):
         toReturn = True
 
         toReturn = toReturn and ("byr" in self.__dataDictionary)
@@ -32,6 +32,65 @@ class Passport:
         toReturn = toReturn and ("ecl" in self.__dataDictionary)
         toReturn = toReturn and ("pid" in self.__dataDictionary)
         #toReturn = toReturn and ("cid" in self.__dataDictionary) # "cid" is optional for part 1
+
+        return toReturn
+
+    def isPassportValidPart2(self):
+        toReturn = self.isPassportValidPart1()
+
+        if toReturn:
+            byr = int(self.__dataDictionary["byr"])
+            toReturn = toReturn and byr >= 1920 and byr <= 2002
+
+            iyr = int(self.__dataDictionary["iyr"])
+            toReturn = toReturn and iyr >= 2010 and iyr <= 2020
+
+            eyr = int(self.__dataDictionary["eyr"])
+            toReturn = toReturn and eyr >= 2020 and eyr <= 2030
+
+            if toReturn:
+                hgt = self.__dataDictionary["hgt"]
+                hgtLength = len(hgt)
+                if hgtLength > 2:
+                    height = int(hgt[:hgtLength - 2])
+                    unit = hgt[hgtLength - 2:hgtLength]
+
+                    if unit == "cm":
+                        toReturn = toReturn and height >= 150 and height <= 193
+                    elif unit == "in":
+                        toReturn = toReturn and height >= 59 and height <= 76
+                    else:
+                        toReturn = False
+                else:
+                    # There must be a 2 character unit + a number in the string
+                    toReturn = False
+
+            if toReturn:
+                hcl = self.__dataDictionary["hcl"]
+                hclLength = len(hcl)
+
+                toReturn = toReturn and hclLength == 7
+                toReturn = toReturn and hcl[0] == '#'
+
+                if toReturn:
+                    for character in range(1, 7):
+                        if (hcl[character] >= '0' and hcl[character] <= '9') or (hcl[character] >= 'a' and hcl[character] <= 'f'):
+                            pass
+                        else:
+                            toReturn = False
+                            break
+
+            if toReturn:
+                ecl = self.__dataDictionary["ecl"]
+                if ecl == "amb" or ecl == "blu" or ecl == "brn" or ecl == "gry" or ecl == "grn" or ecl == "hzl" or ecl == "oth":
+                    pass
+                else:
+                    toReturn = False
+
+            pid = self.__dataDictionary["pid"]
+            toReturn = toReturn and pid.isdigit() and len(pid) == 9
+
+            # don't need to validate cid
 
         return toReturn
 
